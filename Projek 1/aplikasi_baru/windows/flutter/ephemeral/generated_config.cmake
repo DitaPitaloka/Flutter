0b1246fd6a7abe49bc36defe9f5dbd451ace93b9
dita <dith5288@gmail.com>
2024-03-08 09:05:57 +0700

    Project Baru

---
+++ Projek 1/aplikasi_baru/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Study\\Semester 4\\Pengembangan Apk Mobile\\flutter_windows_3.16.9-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Study\\Semester 4\\Pengembangan Apk Mobile\\belajar flutter\\aplikasi_baru" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Study\\Semester 4\\Pengembangan Apk Mobile\\flutter_windows_3.16.9-stable\\flutter"
+  "PROJECT_DIR=C:\\Study\\Semester 4\\Pengembangan Apk Mobile\\belajar flutter\\aplikasi_baru"
+  "FLUTTER_ROOT=C:\\Study\\Semester 4\\Pengembangan Apk Mobile\\flutter_windows_3.16.9-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Study\\Semester 4\\Pengembangan Apk Mobile\\belajar flutter\\aplikasi_baru\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Study\\Semester 4\\Pengembangan Apk Mobile\\belajar flutter\\aplikasi_baru"
+  "FLUTTER_TARGET=C:\\Study\\Semester 4\\Pengembangan Apk Mobile\\belajar flutter\\aplikasi_baru\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9mNDBlOTc2YmVkZmY1N2U2OWUxYjNkODlhN2MyYTNjNjE3YTAzZGFkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Study\\Semester 4\\Pengembangan Apk Mobile\\belajar flutter\\aplikasi_baru\\.dart_tool\\package_config.json"
+)
